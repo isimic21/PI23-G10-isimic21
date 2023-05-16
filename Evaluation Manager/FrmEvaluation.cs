a5840c1c54d991e1c4cdcb0adf84093c32c1c650
pi-vjezbe <pi@outlook.com>
2023-05-16 16:23:45 +0200

    ajme

--- Evaluation Manager/FrmEvaluation.cs
+++ Evaluation Manager/FrmEvaluation.cs
@@ -13,6 +13,7 @@ using System.Windows.Forms;
 namespace Evaluation_Manager {
     public partial class FrmEvaluation : Form {
         private Student student;
+        public Student SelectedStudent { get => student; set => student = value; }
         public FrmEvaluation(Student selectedStudent) {
             InitializeComponent();
             student = selectedStudent;
@@ -38,6 +39,17 @@ namespace Evaluation_Manager {
             txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" + currentActivity.MaxPoints;
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+            var evaluation = EvaluationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if (evaluation != null) {
+                txtTeacher.Text = evaluation.Evaluator.ToString();
+                txtEvaluationDate.Text = evaluation.EvaluationDate.ToString();
+                numPoints.Value = evaluation.Points;
+            } else {
+                txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "-";
+                numPoints.Value = 0;
+            }
         }
 
         private void txtMinForSignature_TextChanged(object sender, EventArgs e) {
@@ -61,11 +73,15 @@ namespace Evaluation_Manager {
         }
 
         private void btnSave_Click(object sender, EventArgs e) {
-
+            var activity = cboActivities.SelectedItem as Activity;
+            var teacher = FrmLogin.LoggedTeacher;
+            int points = (int)numPoints.Value;
+            teacher.PerformEvaluation(SelectedStudent, activity, points);
+            Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
-
+            Close();
         }
 
         private void label2_Click(object sender, EventArgs e) {
---
+++ Evaluation Manager/Models/Evaluation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models {
+    public class Evaluation {
+        public Activity Activity { get; set; }
+        public Student Student { get; set; }
+        public Teacher Evaluator { get; set; }
+        public DateTime EvaluationDate { get; set; }
+        public int Points { get; set; }
+        public bool IsSufficentForGrade() {
+            return Points >= Activity.MinPointsForGrade;
+        }
+        public bool IsSufficentForSignature() {
+            return Points >= Activity.MinPointsForSignature;
+        }
+    }
+}
